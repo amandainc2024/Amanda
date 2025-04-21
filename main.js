3b8d95f108f0f8b6236644601a71f5a41ad25151
amandainc2024 <info@amandainc.xyz>
2025-04-21 15:03:12 +0300

    update

--- main.js
+++ main.js
@@ -29,3 +29,41 @@ const img = document.querySelector('.hero-img');
     dy = (Math.random() - 0.5) * 6;
 
     animate();
+    
+    function shift(what){
+      var img = document.getElementById('viewport');
+      var infop = document.getElementById('ip');
+
+     
+     function  animateimg() {
+       img.style.width='1%';
+       setTimeout(function() {
+         imgS();
+         img.style.width='100%';
+       },300)
+
+     }
+     var mi = ["Building powerful web solutions designed to deliver seamless performance and exceptional user experiences.","We bring creativity and strategy together to deliver designs that not only look stunning but also serve a purpose. Whether it's a logo, brochure, or social media content, our designs amplify your message and elevate your brand.","We combine sleek design with smart functionality to build apps that users love and businesses rely on. Whether it's a customer-facing mobile app or a powerful internal tool, we develop solutions that are intuitive, scalable, and built to perform.","We blend compelling visuals with strategic messaging to craft social media ads that stop the scroll and spark engagement. From Instagram stories to sponsored posts, our ads are designed to connect, convert, and grow your audience."]
+     animateimg()
+      function imgS() {
+        
+      
+      if(what == 'w'){
+        img.src='images/web.jpg';
+        ip.innerText = mi[0]
+      }
+      else if (what == 'a') {
+  img.src = 'images/app.jpg';
+  ip.innerText = mi[2]
+}
+else if (what == 'o') {
+  img.src = 'images/smedia.jpg';
+  ip.innerText = mi[3]
+}
+if (what == 'g') {
+  img.src = 'images/graphics.jpg';
+  ip.innerText = mi[1]
+
+}}
+ 
+    }
