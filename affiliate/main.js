be9f1c20a4137d78f999e1d736a2f108306170e9
amandainc2024 <info@amandainc.xyz>
2025-05-10 21:50:07 +0300

    update

--- affiliate/main.js
+++ affiliate/main.js
@@ -8,3 +8,20 @@ function nav(h,e) {
 
  }
 }
+function c(th){
+ var ans = th.querySelector('.answer');
+ var anss = document.querySelectorAll('.faq .answer');
+ 
+ for(i=0; i<anss.length;i++){
+  anss[i].classList.add('none')
+ }
+ 
+ if(ans.classList.contains('none')){
+  ans.classList.remove('none')
+  
+ }
+ else{
+  ans.classList.add('none')
+ }
+ 
+}
